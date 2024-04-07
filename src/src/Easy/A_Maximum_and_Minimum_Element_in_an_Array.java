89b81ed5f6c246f5284b3f495742a34f9ca78cf2
TornovDutta <162907041+TornovDutta@users.noreply.github.com>
2024-04-07 22:23:55 +0530

    Add files via upload

---
+++ src/src/Easy/A_Maximum_and_Minimum_Element_in_an_Array.java
@@ -0,0 +1,30 @@
+package Easy;
+
+public class A_Maximum_and_Minimum_Element_in_an_Array {
+    private static int max(int arr[]){
+        int max=arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            max=Math.max(max,arr[i]);
+        }
+
+
+
+        return max;
+
+    }
+
+    private static int min(int arr[]){
+        int min=arr[0];
+        for (int i = 0; i <arr.length; i++){
+            min=Math.min(min,arr[i]);
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+       int arr[]={12,7,4,9,2,7,0};
+        System.out.println("max element in array is "+max(arr));
+
+        System.out.println("min element in array is "+min(arr));
+    }
+}
